4b0199d08f79cbbfdd20986735df99f0014007a0
Niscolinx <35029903+Niscolinx@users.noreply.github.com>
2019-02-11 04:26:33 +0100

    adding the media queries

---
+++ Tours/script.js
@@ -0,0 +1,6 @@
+// let clicked = document.querySelector('.clicked')
+
+// clicked.addEventListener("click", function(params) {
+//     alert('Its working as expected');
+//     console.log(count++);
+// })
